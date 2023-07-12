c61d45abc417d8d6ab80bc9500646c86861ca1cc
koshikawa <19rd067@ms.dendai.ac.jp>
2023-07-13 00:30:34 +0900

    fix to connect to DB and get data from it

--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
-  _"fmt"
+  "fmt"
+  "database/sql"
+	"github.com/go-sql-driver/mysql"
+	"time"
   "github.com/syunsukeA/oreno_ramen/golang/internal"
   "github.com/gin-gonic/gin"
 )
 
+func connectDB(dbURL string) *sql.DB {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+	c := mysql.Config{
+		DBName:    "oreno_ramen",
+		User:      "root",
+		Passwd:    "pokemon18782",
+		Addr:      dbURL,
+		// Net:       "tcp",
+		ParseTime: true,
+		// Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+	db, err := sql.Open("mysql", c.FormatDSN())
+  if err != nil {
+    panic(err)
+  }
+  
+  return db
+}
+
 func main() {
+  dbURL := "Local"
+  db := connectDB(dbURL)
+  defer db.Close()
+
+  query := "SELECT * FROM user"
+  rows, err := db.Query(query)
+  var user_id int
+  var name sql.NullString
+  var password sql.NullString
+  for rows.Next() {
+    rows.Scan(&user_id, &name, &password)
+    fmt.Println(user_id, name, password)
+  }
+  if err != nil {
+    panic(err)
+  }
+
 	router := gin.Default()
 	router.GET("/", internal.GetShoplist)
-    router.Run("localhost:8080")
+  router.Run("localhost:8080")
 }
