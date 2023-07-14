511367b8afa54b35c4e8efadca3d443edfddd63b
koshikawa <19rd067@ms.dendai.ac.jp>
2023-07-14 15:03:00 +0900

    update to initialize DB by init.sql on Docker environment

---
+++ sql/init.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `users`;
+create table `users` (
+    `user_id`         BIGINT(20) AUTO_INCREMENT,
+    `user_name`       VARCHAR(36) NOT NULL,
+    `created_at` datetime  default current_timestamp,
+    -- `updated_at` timestamp default current_timestamp on update current_timestamp
+    -- UNIQUE KEY uq_keys(user_name),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+INSERT INTO users (user_name) VALUES ('test_user');
