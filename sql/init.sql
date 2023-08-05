50a0623020db17a0ad35cfdd5ca9371d0cff63e9
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-05 16:00:02 +0900

    update users table definition in init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS `users`;
 create table `users` (
     `user_id`         BIGINT(20) AUTO_INCREMENT,
-    `user_name`       VARCHAR(36) NOT NULL,
+    `profile_img`     text,  -- ToDo: 適切なデータ型決める
+    `username`        VARCHAR(36) NOT NULL, -- ToDo: UNIQUEにする
     `password`        VARCHAR(36) NOT NULL,
     `created_at`      datetime  default current_timestamp,
     -- `updated_at` timestamp default current_timestamp on update current_timestamp
@@ -9,6 +10,6 @@ create table `users` (
     PRIMARY KEY (`user_id`)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-INSERT INTO users (user_name, password) VALUES ('test_user', 'pass');
-INSERT INTO users (user_name, password) VALUES ('syunsuke', 'hoge');
-INSERT INTO users (user_name, password) VALUES ('guest1', '0120');
+INSERT INTO users (username, password) VALUES ('test_user', 'pass');
+INSERT INTO users (username, password) VALUES ('syunsuke', 'hoge');
+INSERT INTO users (username, password) VALUES ('guest1', '0120');
