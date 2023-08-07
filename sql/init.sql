597558b28a1f5186e1c6895621b2fee397d0bb25
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-07 13:16:34 +0900

    add reviews table definition to init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `users`;
+USE `oreno_ramen_db`;
 create table `users` (
     `user_id`         BIGINT(20) AUTO_INCREMENT,
     `profile_img`     text,  -- ToDo: 適切なデータ型決める
@@ -8,8 +8,27 @@ create table `users` (
     `updated_at`      timestamp default current_timestamp on update current_timestamp,
     UNIQUE KEY (`username`),
     PRIMARY KEY (`user_id`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+);
 
+create table `reviews` (
+    `review_id`         BIGINT(20) AUTO_INCREMENT,
+    `user_id`           BIGINT(20) NOT NULL,
+    `shop_id`           BIGINT(20), -- HotPepperのPK
+    `shopname`          VARCHAR(36) NOT NULL,
+    `content`           text, -- ToDo: データ型の選定
+    `evaluate`          INT CHECK (evaluate >= 0 AND evaluate <=5),
+    `review_img`        text,  -- ToDo: 適切なデータ型決める, ToDo: 画像が複数設定可能な場合どうする？
+    `bookmark`          TINYINT(1) NOT NULL, -- 0or1の値 (ToDo: Goの方でBooleanの数値的扱いを確認するべきかも)
+    `created_at`        datetime  default current_timestamp,
+    `updated_at`        timestamp default current_timestamp on update current_timestamp,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    PRIMARY KEY (`review_id`)
+);
+
+-- Initial data for users table
 INSERT INTO users (username, password) VALUES ('test_user', 'pass');
 INSERT INTO users (username, password) VALUES ('syunsuke', 'hoge');
 INSERT INTO users (username, password) VALUES ('guest1', '0120');
+
+-- Initial data for reviews table
+INSERT INTO reviews (user_id, shop_id, shopname, bookmark) VALUES (1, 0, 'test_review', 0);
