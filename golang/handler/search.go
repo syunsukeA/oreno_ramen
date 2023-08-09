381cdfb2e9eb97192b1c2a1cdab3a9e60ba06f5c
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-09 21:52:14 +0900

    update to get param from query "/search/unvisited"

--- golang/handler/search.go
+++ golang/handler/search.go
@@ -16,9 +16,17 @@ type HSearch struct {
 
 func (h *HSearch) SearchUnvisited(c *gin.Context){
 	log.Println("OK!!!!!!!!!!!!!!")
+	r := c.Request
 	w := c.Writer
+	lat := r.URL.Query().Get("lat") 
+	lng := r.URL.Query().Get("lng")
+	rng := r.URL.Query().Get("rng")
+	test_resp := []*object.Review{}
+	test_resp = append(test_resp, &object.Review{})
+
+	log.Printf("lat: %s, lng: %s, rng: %s\n", lat, lng, rng)
 	w.Header().Set("Content-Type", "text/plain")
-	if err := json.NewEncoder(w).Encode(h.RO); err != nil {
+	if err := json.NewEncoder(w).Encode(test_resp); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println(err)
 		return
