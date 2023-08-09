918ab3263820c12f4d6f6f9e29ab4fd63d58556f
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-09 21:52:31 +0900

    add json and db tag

--- golang/domain/object/review.go
+++ golang/domain/object/review.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Review struct {
-	ReviewID int64
-	UserID int64
-	ShopID int64
-	ShopName string
-	Content string
-	Evaluate uint
-	ReviewImg string
-	Bookmark bool
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ReviewID int64 `json:"review_id" db:"review_id"`
+	UserID int64 `json:"user_id" db:"user_id"`
+	ShopID int64 `json:"shop_id" db:"shop_id"`
+	ShopName string `json:"shopname" db:"shopname"`
+	Content string `json:"content" db:"content"`
+	Evaluate uint `json:"evaluate" db:"evaluate"`
+	ReviewImg string `json:"review_img" db:"review_img"`
+	Bookmark bool `json:"bookmark" db:"bookmark"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
