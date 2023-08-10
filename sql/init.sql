51f4a3d8e68f9dd8b31eaeb20ec2c7e65a64664c
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-10 16:52:16 +0900

    add db table 'shops'

--- sql/init.sql
+++ sql/init.sql
@@ -10,6 +10,16 @@ create table `users` (
     PRIMARY KEY (`user_id`)
 );
 
+create table `shops` (
+    `shop_id`           BIGINT(20) PRIMARY -- HotPepperのPK
+    `user_id`           BIGINT(20) NOT NULL,
+    `shopname`          VARCHAR(36) NOT NULL,
+    `bookmark`          TINYINT(1) NOT NULL, -- 0or1の値 (ToDo: Goの方でBooleanの数値的扱いを確認するべきかも)
+    `created_at`        datetime  default current_timestamp,
+    `updated_at`        timestamp default current_timestamp on update current_timestamp,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+);
+
 create table `reviews` (
     `review_id`         BIGINT(20) AUTO_INCREMENT,
     `user_id`           BIGINT(20) NOT NULL,
@@ -18,7 +28,6 @@ create table `reviews` (
     `content`           text, -- ToDo: データ型の選定
     `evaluate`          INT CHECK (evaluate >= 0 AND evaluate <=5),
     `review_img`        text,  -- ToDo: 適切なデータ型決める, ToDo: 画像が複数設定可能な場合どうする？
-    `bookmark`          TINYINT(1) NOT NULL, -- 0or1の値 (ToDo: Goの方でBooleanの数値的扱いを確認するべきかも)
     `created_at`        datetime  default current_timestamp,
     `updated_at`        timestamp default current_timestamp on update current_timestamp,
     FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
