4d46e06a752eceb5b7860e22d847ab1d86e078f2
koshikawa <19rd067@ms.dendai.ac.jp>
2023-08-10 19:56:32 +0900

    update unvisited API able to get info from HP API

--- golang/domain/object/shop.go
+++ golang/domain/object/shop.go
@@ -16,3 +16,40 @@ type Shop struct {
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
+
+type PhotoType struct {
+	L string `json:"l"`
+	M string `json:"m"`
+	S string `json:"s"`
+}
+
+type Photo struct {
+	Mobile PhotoType `json:"mobile"`
+	PC PhotoType `json:"pc"`
+}
+
+type HPShop struct {
+	Access string `json:"access"`
+	Address string `json:"address"`
+	Close string `json:"close"`
+	ID string `json:"id"`
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+	Midinght string `json:"midinght"`
+	MobileAccess string `json:"mobile_access"`
+	Name string `json:"name"`
+	Open string `json:"open"`
+	Photo Photo `json:"photo"`
+}
+
+type HPResult struct {
+	APIVersion string `json:"-"`
+	ResultsAvailable int8 `json:"-"`
+	ResultsReturuned string `json:"-"`
+	ResultsStart int8 `json:"-"`
+	Shop []*HPShop `json:"shop"`
+
+}
+type HPResponse struct {
+	Result HPResult `json:"results"`
+}
--- golang/handler/search.go
+++ golang/handler/search.go
@@ -2,32 +2,74 @@ package handler
 
 import (
 	"log"
+	"io"
 	"net/http"
+	"net/url"
 	"encoding/json"
 
 	"github.com/syunsukeA/oreno_ramen/golang/domain/object"
+	"github.com/syunsukeA/oreno_ramen/golang/domain/repository"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	HP_API_KEY = "b6507930d6c151bd"
+)
+
 type HSearch struct {
-	RO object.Review
+	Sr repository.Shop
+	// Rr repository.Review
 }
 
 func (h *HSearch) SearchUnvisited(c *gin.Context){
-	log.Println("OK!!!!!!!!!!!!!!")
+	// queryパラメータ判定
 	r := c.Request
 	w := c.Writer
 	lat := r.URL.Query().Get("lat") 
 	lng := r.URL.Query().Get("lng")
 	rng := r.URL.Query().Get("rng")
-	test_resp := []*object.Review{}
-	test_resp = append(test_resp, &object.Review{})
+	// ToDo: paramエラーハンドリング
 
-	log.Printf("lat: %s, lng: %s, rng: %s\n", lat, lng, rng)
-	w.Header().Set("Content-Type", "text/plain")
-	if err := json.NewEncoder(w).Encode(test_resp); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+	// HotPepper API呼び出し
+	params := url.Values{}
+    params.Add("key", HP_API_KEY)
+	params.Add("keyword", "ラーメン")
+    params.Add("lat", lat)
+	params.Add("lng", lng)
+	params.Add("range", rng)
+	params.Add("format", "json")
+
+	urls := "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?" + params.Encode()
+	resp, err := http.Get(urls)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	byteData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	hpPesp := new(object.HPResponse)
+	json.Unmarshal(byteData, &hpPesp)
+	hpShops := hpPesp.Result.Shop
+
+	// 検索結果+DB情報からvisitedを消去
+	// ToDo: repository.shopに、外部APIから検索したshop_idを受け取ってunvisitedなshop_idを返す関数を宣言
+
+	// ToDo: unvisited_idの中にない店舗情報を削除するような実装
+	for _, shop := range hpShops {
+		log.Println(shop.ID)
+	}
+
+	// ResponseBodyに書き込み
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(hpShops); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Println(err)
 		return
 	}
