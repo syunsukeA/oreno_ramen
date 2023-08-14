5f5c9e9801fe6bdabbba0d9ffa23b2f1279d6ea5
Jun <kumejun2000@gmail.com>
2023-08-14 15:19:17 +0900

    SignupByUsernameの追加

--- golang/dao/user.go
+++ golang/dao/user.go
@@ -15,7 +15,22 @@ type User struct {
 	DB *sqlx.DB
 }
 
-func (r *User)FindByUsername(c *gin.Context, username string) (uo *object.User, err error) {
+func (r *User) FindByUsername(c *gin.Context, username string) (uo *object.User, err error) {
+	uo = new(object.User)
+	q := `SELECT * from users where username = ?`
+	err = r.DB.QueryRowxContext(c, q, username).StructScan(uo)
+	if err != nil {
+		log.Println("=========================================")
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return uo, nil
+}
+
+func (r *User) SignupByUsername(c *gin.Context, username string) (uo *object.User, err error) {
 	uo = new(object.User)
 	q := `SELECT * from users where username = ?`
 	err = r.DB.QueryRowxContext(c, q, username).StructScan(uo)
