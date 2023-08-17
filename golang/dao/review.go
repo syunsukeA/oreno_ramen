8b45f2a500559c977d10a5489b754fca817d32b0
Jun <kumejun2000@gmail.com>
2023-08-17 09:09:34 +0900

    構造体を引数にするよう修正

--- golang/dao/review.go
+++ golang/dao/review.go
@@ -1,13 +1,14 @@
 package dao
 
 import (
-	_"log"
-	_"reflect"
-	"errors"
 	"database/sql"
+	"errors"
+	_ "log"
+	_ "reflect"
 
 	"github.com/gin-gonic/gin"
 	"github.com/syunsukeA/oreno_ramen/golang/domain/object"
+	"github.com/syunsukeA/oreno_ramen/golang/domain/repository"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -17,31 +18,35 @@ type Review struct {
 	DB *sqlx.DB
 }
 
-func (r *Review)GetUnvisitedReviews() (ROs []*object.Review){
+func (r *Review) GetUnvisitedReviews() (ROs []*object.Review) {
 	entity := new(object.Review)
 	ROs = append(ROs, entity)
 	return ROs
 }
 
-func (r *Review)AddReviewAndShop(c *gin.Context, shopID string, userID int64, shopname string, req *object.CreateReviewRequest) (ro *object.Review, err error) {
+func (r *Review) AddReviewAndShop(c *gin.Context, rev_info *repository.AddReviewInput) (ro *object.Review, err error) {
+	shopID := rev_info.ShopID
+	userID := rev_info.UserID
+	shopname := rev_info.ShopName
+	req := rev_info.Request
 	ro = new(object.Review)
 	// トランザクション処理の開始
 	tx, err := r.DB.BeginTxx(c, nil)
 	if err != nil {
-        return nil, err
-    }
+		return nil, err
+	}
 	defer func() (*object.Review, error) {
-        switch r := recover(); {
+		switch r := recover(); {
 		case r != nil:
-            tx.Rollback()
-            return nil, err
-        case err != nil:
-            tx.Rollback()
+			tx.Rollback()
+			return nil, err
+		case err != nil:
+			tx.Rollback()
 			return nil, err
 		default:
 			return ro, nil
 		}
-    }()
+	}()
 	so := new(object.Shop)
 	// shopに該当データがあるか確認
 	q := `SELECT * from shops WHERE shop_id = ?`
@@ -69,22 +74,22 @@ func (r *Review)AddReviewAndShop(c *gin.Context, shopID string, userID int64, sh
 	// ToDo: 構造体駆使して短くする？
 	q = `INSERT INTO reviews (user_id, shop_id, shopname, dishname, content, evaluate, review_img) VALUES (?, ?, ?, ?, ?, ?, ?)`
 	res, err := tx.Exec(q, userID, shopID, shopname, req.DishName, req.Content, req.Evaluate, req.ReviewImg)
-		if err != nil {
-			return nil, err
-		}
-		// ToDo: 作成したreviewをResBodyに入れるために検索？？
-		// review_id, err := res.LastInsertId()
-		if err != nil {
-			return nil, err
-		}
-		// ToDo: もしかしたらこの辺の処理いらないかも？
-		nrows, err := res.RowsAffected()
-		if err != nil {
-			return nil, err
-		}
-		if nrows <= 0 {
-			return nil, sql.ErrNoRows
-		}
+	if err != nil {
+		return nil, err
+	}
+	// ToDo: 作成したreviewをResBodyに入れるために検索？？
+	// review_id, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	// ToDo: もしかしたらこの辺の処理いらないかも？
+	nrows, err := res.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+	if nrows <= 0 {
+		return nil, sql.ErrNoRows
+	}
 	// トランザクション処理をコミット
 	if err = tx.Commit(); err != nil {
 		return nil, err
@@ -92,7 +97,7 @@ func (r *Review)AddReviewAndShop(c *gin.Context, shopID string, userID int64, sh
 	return ro, nil
 }
 
-func (r *Review)FindReviewsByShopID(c *gin.Context, userID int64, shopID string) (ros []*object.Review, err error){
+func (r *Review) FindReviewsByShopID(c *gin.Context, userID int64, shopID string) (ros []*object.Review, err error) {
 	ros = []*object.Review{}
 	ro := new(object.Review)
 	q := `SELECT * FROM reviews WHERE user_id = ? AND shop_id = ? LIMIT 20`
@@ -112,7 +117,7 @@ func (r *Review)FindReviewsByShopID(c *gin.Context, userID int64, shopID string)
 	return ros, err
 }
 
-func (r *Review)UpdateReview(c *gin.Context, ro *object.Review) (roPost *object.Review, err error) {
+func (r *Review) UpdateReview(c *gin.Context, ro *object.Review) (roPost *object.Review, err error) {
 	roPost = new(object.Review)
 	q := `
 		UPDATE reviews
